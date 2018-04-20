f723387f73537d740f471e0a6795a90fd35c6025
wshngyf <8769634@qq.com>
2018-04-20 23:58:15 +0800

    创建车牌管理模块

--- SimpleWeb/SimpleWeb/urls.py
+++ SimpleWeb/SimpleWeb/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from LicensePlateManage import views
 
 urlpatterns = [
+    path('index/',views.index),
     path('admin/', admin.site.urls),
 ]
