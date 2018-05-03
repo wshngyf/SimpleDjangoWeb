bd75060c459025d0315ac2a6248d8236895e5138
wshngyf <8769634@qq.com>
2018-05-03 23:11:14 +0800

    实现分页

--- SimpleWeb/LicensePlateManage/views.py
+++ SimpleWeb/LicensePlateManage/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
-from django.http import JsonResponse,HttpResponse
+from django.http import HttpResponse
 from LicensePlateManage.models import LicensePlate
 from LicensePlateManage.serializer import LicenseSerializers
-import re
 import json
-import demjson
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def index(request):
     licenseplate = LicensePlate.objects.all()
-    serializer = LicenseSerializers(licenseplate, many=True)
+    pageSize = request.GET.get('pageSize')
+    page=int(request.GET.get('offset'))
+    paginator = Paginator(licenseplate, pageSize)
+    page=page/5+1
+    serializer = LicenseSerializers(paginator.page(page), many=True)
+
     con=serializer.data
-    str=json.dumps({'rows': con,'page':1,'total':20})
+    str=json.dumps({'rows': con,'page':page,'total':paginator.count})
     #return render(request, "index.html")
     #str={"rows": [{"province": "豫","city": "A","license": "00001","licenseplate": "豫A00001","phonenum": "13939112345","carnum": "2341243235345"}],"page": 1,"total": 1}
     return HttpResponse(str)
